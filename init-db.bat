2257480cc86d28a1b146907a41543ead56e9c3fe
Kent Jasper Cabunoc Sisi <2081367@g.cu.edu.ph>
2025-03-15 00:16:38 +0800

    delete

--- init-db.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-echo Installing dependencies...
-call npm install
-
-echo Initializing database...
-call npm run init-db
-
-pause 
--- init-db.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# Install dependencies
-echo "Installing dependencies..."
-npm install
-
-# Initialize database
-echo "Initializing database..."
-npm run init-db 
